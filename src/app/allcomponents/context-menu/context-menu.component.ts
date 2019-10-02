1d5bd4f006e138707511e6c204271f879e1e409f
Eduardo <sheka1990@gmail.com>
2019-10-02 15:40:52 -0600

    fix show pop up and context menu

--- src/app/allcomponents/context-menu/context-menu.component.ts
+++ src/app/allcomponents/context-menu/context-menu.component.ts
@@ -1,34 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { getInstance } from '@syncfusion/ej2-base';
-import { MenuItemModel, ContextMenu } from '@syncfusion/ej2-navigations';
+import { Component } from '@angular/core';
+import { MenuItemModel } from '@syncfusion/ej2-navigations';
+
 @Component({
   selector: 'app-context-menu',
-  template: `<!-- To Render ContextMenu. -->
-            <ejs-contextmenu id='contextmenu' [items]= 'menuItems'></ejs-contextmenu>
-            <!-- To Render Button. -->
-            <button ejs-button (click)="btnClick()">Open ContextMenu</button>`,
+  template: `<!--target element-->
+  <div id="target">Right click / Touch hold to open the ContextMenu</div>
+
+  <!-- To Render ContextMenu. -->
+  <ejs-contextmenu id='contextmenu' target='#target' [items]= 'menuItems'></ejs-contextmenu>`,
   styleUrls: ['./context-menu.component.scss']
 })
-export class ContextMenuComponent implements OnInit {
- // Initialize action items.
- public menuItems: MenuItemModel[] = [
-  {
-      text: 'Cut'
-  },
-  {
-      text: 'Copy'
-  },
-  {
-      text: 'Paste'
-  }];
+export class ContextMenuComponent{
 
-btnClick() {
-  let contextmenuObj: ContextMenu = getInstance(document.getElementById("contextmenu_0"), ContextMenu) as ContextMenu;
-  contextmenuObj.open(40, 20);
-}
-  constructor() { }
-
-  ngOnInit() {
-  }
+  public menuItems: MenuItemModel[] = [
+    {
+        text: 'Cut'
+    },
+    {
+        text: 'Copy'
+    },
+    {
+        text: 'Paste'
+    }];
 
 }
--- src/app/allcomponents/popup/popup.component.ts
+++ src/app/allcomponents/popup/popup.component.ts
@@ -6,8 +6,11 @@ import { EmitType } from '@syncfusion/ej2-base';
   selector: 'app-popup',
   template: `<button class="e-control e-btn" style="position: absolute;" id="targetButton" (click)="onOpenDialog($event)">Open Dialog</button>
   <div #container class='root-container'>
-    <ejs-dialog id='dialog' #ejDialog [animationSettings]='animationSettings' [closeOnEscape]='closeOnEscape' content='Are you sure you want to permanently delete all of these items?' header='Delete Multiple Items' [target]='targetElement'
-    width='486px' [showCloseIcon]='showCloseIcon' [buttons]='buttons'>
+    <ejs-dialog [visible]='visible' id='dialog' #ejDialog [animationSettings]='animationSettings' [closeOnEscape]='closeOnEscape' content='Are you sure you want to permanently delete all of these items?' header='Delete Multiple Items' [target]='targetElement'
+    width='486px' [showCloseIcon]='showCloseIcon' [buttons]='buttons' style="    margin: auto;
+    top: 0;
+    bottom: 0;
+    max-height: 200px;">
     </ejs-dialog> </div> `,
   styleUrls: ['./popup.component.scss']
 })
@@ -39,6 +42,7 @@ export class PopupComponent implements OnInit {
   public animationSettings: Object = { effect: 'Zoom', duration: 400, delay: 0 };
 
   public showCloseIcon: boolean = true;
+  public visible: Boolean = false;
   // Hide the Dialog when click the footer button.
   public hideDialog: EmitType<object> = () => {
       this.ejDialog.hide();
