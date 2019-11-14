7dd79d463ae27f36b62cc50eb1ae47b7adf87948
Eduardo <sheka1990@gmail.com>
2019-11-14 14:30:53 -0600

    Footer fixed error

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit, OnInit } from "@angular/core";
 import { SafeHtml, DomSanitizer } from "@angular/platform-browser";
+import * as $ from "jquery";
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
@@ -10,56 +11,73 @@ import { SafeHtml, DomSanitizer } from "@angular/platform-browser";
 
 //   ngOnInit() {}
 // }
-export class DashboardComponent {
-  
-  text = "&nbsp &nbsp &nbsp Bolsa de Valores de El Salvador®. Todos los derechos reservado";
-  tabIndex = 0 ;
-  onTabClick(index){
-        this.tabIndex = index;
-        if (index === 2) {
-          this.toggleMicroTwo();
+export class DashboardComponent implements OnInit {
+  public ngOnInit() {
+    $(document).ready(function() {
+      $("#sidebar li a.dropdown-toggle").on("click", function() {
+        $("#sidebar li.dropdown-sub").addClass("dropdown-inactive");
+
+        $("#sidebar li.dropdown-sub").removeClass("dropdown-active");
+
+        if ($(this.parentNode).hasClass("dropdown-inactive")) {
+          console.log("TRUE");
+          $(this.parentNode).addClass("dropdown-active");
+          // $(this.parentNode).removeClass("dropdown-inactive");
+        } else {
+          console.log("FALSE");
+          // $(this.parentNode).addClass("dropdown-inactive");
+          // $(this.parentNode).removeClass("dropdown-active");
         }
-
-        // cierro el sidebar al dar click en algun link de la pagina
-        this.toggle_sidebar();
-   }
-   title = 'micro-front';
-
-  toShow: SafeHtml = '';
-
-  constructor(private sanitizer: DomSanitizer) {
+      });
+    });
   }
+  text =
+    "&nbsp &nbsp &nbsp Bolsa de Valores de El Salvador®. Todos los derechos reservado";
+  tabIndex = 0;
+  onTabClick(index) {
+    this.tabIndex = index;
+    if (index === 2) {
+      this.toggleMicroTwo();
+    }
+
+    // cierro el sidebar al dar click en algun link de la pagina
+    this.toggle_sidebar();
+  }
+  title = "micro-front";
+
+  toShow: SafeHtml = "";
+
+  constructor(private sanitizer: DomSanitizer) {}
 
   // agrego una variable para validar y poder control sobre el toggle del sidebar
   status: boolean = true;
   // Agregamos una funcion en la cual validaremos el estado del sidebar , esta funcion se debe de agregar en el evento (click) para que se cierre automaticamente
-    toggle_sidebar(){
-      // valido que entra a la funcion
-      this.status = !this.status;  
-      // veo que este realizando el cambio de estado
-      console.log(this.status);  
+  toggle_sidebar() {
+    // valido que entra a la funcion
+    this.status = !this.status;
+    // veo que este realizando el cambio de estado
+    console.log(this.status);
   }
-  private loadScript(url: string): void {
-    
 
-    //cargamos el escript que nos ayudara a mostrar a nuestros componentes 
+  toggle_dropdown() {}
+  private loadScript(url: string): void {
+    //cargamos el escript que nos ayudara a mostrar a nuestros componentes
     if (document.querySelectorAll(`script[src='${url}']`).length === 0) {
-      const script = document.createElement('script');
-      script.onload = function () {
-          // do stuff with the script
+      const script = document.createElement("script");
+      script.onload = function() {
+        // do stuff with the script
       };
       script.src = url;
       document.head.appendChild(script);
     }
   }
 
-   public toggleMicroOne() {
-
+  public toggleMicroOne() {
     // Componente compilado como un microcomponete de Login
     // if (!this.toShow) {
-      console.log('alerta');
-      this.loadScript('elements/micro-one.js');
-      this.toShow = this.sanitizer.bypassSecurityTrustHtml(`<micro-one>
+    console.log("alerta");
+    this.loadScript("elements/micro-one.js");
+    this.toShow = this.sanitizer.bypassSecurityTrustHtml(`<micro-one>
       <div class="loader-05"></div>
       </micro-one>`);
     // } else {
@@ -70,9 +88,9 @@ export class DashboardComponent {
   public toggleMicroTwo() {
     // Componente compilado como un microcomponete de Error
     // if (!this.toShow) {
-      console.log('alerta');
-      this.loadScript('elements/micro-two.js');
-      this.toShow = this.sanitizer.bypassSecurityTrustHtml(`<micro-two>
+    console.log("alerta");
+    this.loadScript("elements/micro-two.js");
+    this.toShow = this.sanitizer.bypassSecurityTrustHtml(`<micro-two>
       <div class="loader-05"></div>
       </micro-two>`);
     // } else {
@@ -80,4 +98,3 @@ export class DashboardComponent {
     // }
   }
 }
-
